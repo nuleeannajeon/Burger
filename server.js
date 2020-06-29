6284b54fa5312a5353a9205938817651bd38ebb1
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-06-28 20:01:43 -0400

    setup server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+require( 'dotenv' ).config();
+
 const express = require('express');
 const exphbs = require("express-handlebars");
+
+const controllerRouter = require('./controllers/burgers_controller')
+const mySQL_PORT = process.env.MYSQL_PORT || 8080;
 
 const app = express();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.listen(PORT, function() {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+controllerRouter(app);
+
+app.use( express.static('public') );
+
+app.listen(mySQL_PORT, function() {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on: http://localhost:" + mySQL_PORT);
 });
