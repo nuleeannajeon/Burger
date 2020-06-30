c7f83e4f6a277b799eeb2c2381c22b179015231b
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-06-29 21:48:16 -0400

    change to port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const exphbs = require("express-handlebars");
 
 const controllerRouter = require('./controllers/burgers_controller')
-const mySQL_PORT = process.env.MYSQL_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -18,7 +18,7 @@ controllerRouter(app);
 
 app.use( express.static('public') );
 
-app.listen(mySQL_PORT, function() {
+app.listen(PORT, function() {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + mySQL_PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
